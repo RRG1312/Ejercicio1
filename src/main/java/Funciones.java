27f6750e77074bad580c00149efea0475ac1e20d
ruben <ruben.ben111@gmail.com>
2023-03-16 19:18:15 +0100

    Terminado

--- src/main/java/Funciones.java
+++ src/main/java/Funciones.java
@@ -1,8 +1,19 @@
 public class Funciones {
-    public static void funcion1(){
-
+    public static int funcion1(int x, int y){
+        if (y == 0) {
+            return 1;
+        } else {
+            return x * funcion1(x, y - 1);
+        }
     }
-    public static void funcion2(){
-
+    public static int funcion2(int y,int x){
+        if (y == 0) {
+            return 1;
+        } else if (y % 2 == 0) {
+            int temp = funcion2(x, y / 2);
+            return temp * temp;
+        } else {
+            return x * funcion2(x, y - 1);
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Dime la x");
+        int x = teclado.nextInt();
+
+        System.out.println("Dime la y");
+        int y= teclado.nextInt();
+
+
+        System.out.println(Funciones.funcion1(x,y));
+        System.out.println(Funciones.funcion2(x,y));
+    }
 }

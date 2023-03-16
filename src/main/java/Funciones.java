64afd27ba39fa85ad6050d8228f6e406023098c9
ruben <ruben.ben111@gmail.com>
2023-03-16 20:03:04 +0100

    Terminado pero de verdad

--- src/main/java/Funciones.java
+++ src/main/java/Funciones.java
@@ -9,11 +9,13 @@ public class Funciones {
     public static double funcion2(int y,int x){
         if (y == 0) {
             return 1;
-        } else if (y % 2 == 0) {
-            double temp = funcion2(x, y / 2);
-            return temp * temp;
         } else {
-            return x * funcion2(x, y - 1);
+            if(y % 2 ==0){
+                return funcion2(x*x,y/2);
+            }else{
+                return x * funcion2(x*x,(y-1)/2);
+            }
+
         }
     }
 
